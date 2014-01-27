687bcfb7dbd66678322ddcda0c1fad389222c8fb
Christopher Ferris <cferris@google.com>
2014-01-26 21:36:13 -0800

    ART: Speed up thread stack tracing for ANRs
    Share a BacktraceMap for all threads being dumped, which speeds up
    ANR dumping. Results from Nexus 9 (average of five):
    
    Before: 0.587s
    After:  0.206s
    
    Change-Id: Ia70e0dbd39049318c02de561e7b95258d4849467

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -732,9 +732,9 @@ void Thread::ShortDump(std::ostream& os) const {
      << "]";
 }
 
-void Thread::Dump(std::ostream& os) const {
+void Thread::Dump(std::ostream& os, BacktraceMap* backtrace_map) const {
   DumpState(os);
-  DumpStack(os);
+  DumpStack(os, backtrace_map);
 }
 
 mirror::String* Thread::GetThreadName(const ScopedObjectAccessAlreadyRunnable& soa) const {
@@ -1211,7 +1211,7 @@ void Thread::DumpJavaStack(std::ostream& os) const {
   }
 }
 
-void Thread::DumpStack(std::ostream& os) const {
+void Thread::DumpStack(std::ostream& os, BacktraceMap* backtrace_map) const {
   // TODO: we call this code when dying but may not have suspended the thread ourself. The
   //       IsSuspended check is therefore racy with the use for dumping (normally we inhibit
   //       the race with the thread_suspend_count_lock_).
@@ -1226,7 +1226,7 @@ void Thread::DumpStack(std::ostream& os) const {
     // If we're currently in native code, dump that stack before dumping the managed stack.
     if (dump_for_abort || ShouldShowNativeStack(this)) {
       DumpKernelStack(os, GetTid(), "  kernel: ", false);
-      DumpNativeStack(os, GetTid(), "  native: ", GetCurrentMethod(nullptr, !dump_for_abort));
+      DumpNativeStack(os, GetTid(), backtrace_map, "  native: ", GetCurrentMethod(nullptr, !dump_for_abort));
     }
     DumpJavaStack(os);
   } else {
